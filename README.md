44627f451ce1763046a0419085c0c2a80fef7d4f
daniel <kontakt@daniel-kahl.de>
2019-04-04 00:27:04 +0200

    development

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # vue-app-helper
+
+## Installation
+
+```bash
+npm install
+```
+
+## usage
+
+```js
+// in your entry point
+
+import Vue from "vue-router";
+import router from "./router";
+import VueAppHelper, { registerApp, mageRoute } from "vue-app-helper";
+
+Vue.use(VueAppHelper, { router });
+
+registerApp("backend", {
+  routes: [
+    // add your routes here like
+    makeRoute(
+      "login",
+      "backend_login",
+      () => import(/* webpackChunkName: "backend" */ "./views/LoginView.vue"),
+      {
+        title: "Login"
+      }
+    )
+  ],
+  hooks: {
+    initial() {
+      // do something on first route enter
+    },
+    enter() {
+      // do something on any route enter
+    },
+    leave() {
+      // do something on any route leave
+    }
+  }
+});
+```
---
+++ src/vue-app-helper.js
@@ -0,0 +1,65 @@
+let router = null;
+
+export default {
+  install(Vue, config = {}) {
+    router = config.router;
+    if (!router) {
+      throw "vue-app-helper needs vue-router instance.";
+    }
+  }
+};
+
+export function registerApp(name, { routes = [], hooks = {} } = {}) {
+  var initialized = false;
+
+  router.addRoutes(
+    routes.map(route => {
+      if (!route.meta) {
+        route.meta = {};
+      }
+      route.meta.appName = name;
+    })
+  );
+
+  router.beforeEach(async (to, from, next) => {
+    let mergedMetaTo = mergeRouteMeta(from.to);
+    let mergedMetaFrom = mergeRouteMeta(from.meta);
+    let isEnter =
+      mergedMetaFrom.appName !== name && mergedMetaTo.appName === name;
+    let isLeave =
+      !isEnter &&
+      mergedMetaFrom.appName !== name &&
+      mergedMetaTo.appName === name;
+
+    if (isEnter && !initialized) {
+      if (hooks.initialize instanceof Function) {
+        await hooks.initialize();
+      }
+      initialized = true;
+    }
+
+    if (isEnter && hooks.enter instanceof Function) {
+      await hooks.enter();
+    }
+
+    if (isLeave && hooks.leave instanceof Function) {
+      await hooks.leave();
+    }
+
+    next();
+  });
+}
+
+export function makeRoute(path, name, component, meta = {}) {
+  return { name, path, component, meta, props: true };
+}
+
+export function mergeRouteMeta(meta) {
+  if (!meta) {
+    meta = {};
+  }
+  return (meta.matches || []).reduce(
+    (result, meta) => ({ ...result, ...meta }),
+    {}
+  );
+}
